40682686b3abb224dd96dee7dcfe92a43f9d6242
lesterrin <voldemar1810@gmail.com>
2023-03-13 15:25:59 +1000

    Незначительные изменения в стилях

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,22 +1,20 @@
-import s from './app.module.css';
 import {Routes, Route} from "react-router-dom"
 import Header from "../header/header";
 import React from "react";
 import Home from "../pages/home/home";
 import Characters from "../pages/characters/characters";
 import NotFound from "../pages/not-found/not-found";
-import Footer from "../footer/footer";
 
 function App() {
     return (
-        <div>
+        <>
             <Header/>
             <Routes>
                 <Route path="/" element={<Home />}/>
                 <Route path="/characters" element={<Characters/>}/>
                 <Route path="*" element={<NotFound />}/>
             </Routes>
-        </div>
+        </>
     );
 }
 
